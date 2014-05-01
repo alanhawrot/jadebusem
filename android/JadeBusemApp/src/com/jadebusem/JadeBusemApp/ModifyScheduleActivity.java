6c168a30b899e7d886f71391e083203a790feb4b
Alan Hawrot <alan.hawrot@gmail.com>
2014-05-01 17:35:43 +0200

    Revert "Filling views with data when navigating up from ModifyScheduleActivity to ScheduleDetailsActivity"
    This reverts commit 96907fd817fdb59bf688f1f2b5c2c3c82c46bde4.

--- android/JadeBusemApp/src/com/jadebusem/JadeBusemApp/ModifyScheduleActivity.java
+++ android/JadeBusemApp/src/com/jadebusem/JadeBusemApp/ModifyScheduleActivity.java
@@ -1,7 +1,6 @@
 package com.jadebusem.JadeBusemApp;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
@@ -10,20 +9,12 @@ import android.widget.TextView;
  */
 public class ModifyScheduleActivity extends Activity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		TextView textView = new TextView(this);
-		textView.setText("Tutaj będzie modyfikowany rozkład (formularz do wypełnienia)");
-		setContentView(textView);
-	}
-
-	@Override
-	public boolean navigateUpToFromChild(Activity child, Intent upIntent) {
-		Intent intent = getIntent();
-		upIntent.putExtra(ScheduleDetailsActivity.SCHEDULE,
-				intent.getSerializableExtra(ScheduleDetailsActivity.SCHEDULE));
-		return super.navigateUpToFromChild(child, upIntent);
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TextView textView = new TextView(this);
+        textView.setText("Tutaj będzie modyfikowany rozkład (formularz do wypełnienia)");
+        setContentView(textView);
+    }
 
 }
