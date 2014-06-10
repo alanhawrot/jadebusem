e3da8ebcf58df1759d4d3a9a84446a961aedd38a
Alan Hawrot <alan.hawrot@gmail.com>
2014-06-10 19:35:57 +0200

    memcached added

--- site/jadebusem/settings.py
+++ site/jadebusem/settings.py
@@ -47,26 +47,26 @@ DATABASES = {
     # }
 
     ## Alan's local db
-    'local': {
-       'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-       'NAME': '/home/alanhawrot/Dokumenty/sqlite.db',                      # Or path to database file if using sqlite3.
-       # The following settings are not used with sqlite3:
-       'USER': '',
-       'PASSWORD': '',
-       'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-       'PORT': '',
-    }
+    # 'local': {
+    #    'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+    #    'NAME': '/home/alanhawrot/Dokumenty/sqlite.db',                      # Or path to database file if using sqlite3.
+    #    # The following settings are not used with sqlite3:
+    #    'USER': '',
+    #    'PASSWORD': '',
+    #    'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+    #    'PORT': '',
+    # }
 
     ## Michal's local db
-    # 'local': {
-    #     'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-    #     'NAME': os.path.dirname(os.path.dirname(__file__)) + "/db.sqlite",                      # Or path to database file if using sqlite3.
-    #     # The following settings are not used with sqlite3:
-    #     'USER': '',
-    #     'PASSWORD': '',
-    #     'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-    #     'PORT': '',
-    # }
+    'local': {
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': os.path.dirname(os.path.dirname(__file__)) + "/db.sqlite",                      # Or path to database file if using sqlite3.
+        # The following settings are not used with sqlite3:
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '',
+    }
 }
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
